f45b054dff3048a44f34f15a2baf5376de463ece
whyalwaysme <coworasavanila@gmail.com>
2023-05-01 20:29:10 +0700

    feat: slicing dashboard

--- resources/js/Layouts/Auth/Sidebar.jsx
+++ resources/js/Layouts/Auth/Sidebar.jsx
@@ -149,15 +149,15 @@ const Sidebar = () => {
                         </a>
                     </div>
 
-                    <div className="mt-auto pr-[30px]">
-                        <div className="p-5 bg-white rounded-[25px] outline outline-1 outline-[#f1f1f1]">
-                            <div className="text-black text-lg font-semibold mb-8">
+                    <div className="mt-auto pb-5 pr-[30px]">
+                        <div className="p-5 bg-black rounded-[25px] outline outline-1 outline-[#f1f1f1]">
+                            <div className="text-white text-lg font-semibold mb-8">
                                 Basic
                             </div>
-                            <div className="text-black text-sm mb-2">
+                            <div className="text-white text-sm mb-2">
                                 5 of 30 hari
                             </div>
-                            <div className="rounded-full w-full h-[6px] bg-[#f1f1f1]">
+                            <div className="rounded-full w-full h-[6px] bg-[#696969]">
                                 <div className="rounded-full h-full w-2/12 bg-alerange"></div>
                             </div>
                         </div>
--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -1,12 +1,63 @@
 import { Head } from "@inertiajs/react";
-
 import Authenticated from "@/Layouts/Auth/Index";
+import Flickity from "react-flickity-component";
+import FeaturedMovie from "@/Components/FeaturedMovie";
+import MovieCard from "@/Components/MovieCard";
 
 const Dashboard = () => {
+    const flickityOptions = {
+        cellAlign: "left",
+        contain: true,
+        groupCells: 1,
+        wrapAround: false,
+        pageDots: false,
+        prevNextButtons: false,
+        draggable: ">1",
+    };
+
     return (
         <>
-            <Head title="Dashboard" />
-            <Authenticated />
+            <Head title="Dashboard">
+                <link
+                    rel="stylesheet"
+                    href="https://unpkg.com/flickity@2/dist/flickity.min.css"
+                />
+            </Head>
+            <Authenticated>
+                <div>
+                    <div className="font-semibold text-[22px] text-black mb-4">
+                        Featured Movies
+                    </div>
+                    <Flickity className="gap-[30px]" options={flickityOptions}>
+                        {[1, 2, 3, 4].map((i) => (
+                            <FeaturedMovie
+                                key={i}
+                                slug="the-batman-in-love"
+                                name={`The Batman in Love ${i}`}
+                                category="Action Horror"
+                                thumbnail="https://picsum.photos/id/1/300/300"
+                                rating={i + 1}
+                            />
+                        ))}
+                    </Flickity>
+                </div>
+                <div className="mt-[50px]">
+                    <div className="font-semibold text-[22px] text-black mb-4">
+                        Browse
+                    </div>
+                    <Flickity className="gap-[30px]" options={flickityOptions}>
+                        {[1, 2, 3, 4, 5, 6].map((i) => (
+                            <MovieCard
+                                key={i}
+                                slug="the-batman-in-love"
+                                name={`The Batman in Love ${i}`}
+                                category="Action Horror"
+                                thumbnail="https://picsum.photos/id/1/300/300"
+                            />
+                        ))}
+                    </Flickity>
+                </div>
+            </Authenticated>
         </>
     );
 };
